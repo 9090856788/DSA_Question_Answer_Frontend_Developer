b77f305b552269332327b11f332f9bccb361a97e
Kanhu@143 <kanhucharansahoo595@gmail.com>
2024-06-26 03:05:25 +0530

    added 2 nd approach to solve sum of natural number from 1 to n.

--- Basic/SumOfNaturalNumber.js
+++ Basic/SumOfNaturalNumber.js
@@ -1,9 +1,15 @@
-// Q - Write a program to calculate sum of all natural number from 1 to n.
-const SumOfNaturalNumber = (n) => {
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
-    sum = sum + i;
-  }
-  console.log("Sum of Natural Number is : ", sum);
-};
-SumOfNaturalNumber(5);
+//  Q - Write a program to calculate sum of all natural number from 1 to n.
+// const SumOfNaturalNumber = (n) => {
+//   First Approach
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++) {
+//       sum = sum + i;
+//     }
+//     console.log("Sum of Natural Number is : ", sum);
+
+//   2nd approach
+//   let sum = (n * (n + 1)) / 2;
+//   return sum;
+// };
+// let ans = SumOfNaturalNumber(5);
+// console.log("Sum of Natural Number is : ", ans);
