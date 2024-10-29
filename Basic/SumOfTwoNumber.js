02214db0148a0d6bef46f01cf28d11ff045fa612
9090856788 <kanhucharansahoo595@gmail.com>
2024-10-29 16:32:48 +0530

    Write a javascript function to find the sum of two number.

---
+++ Basic/SumOfTwoNumber.js
@@ -0,0 +1,9 @@
+// Write a javascript function to find the sum of two number.
+
+const SumOfTwoNumber = (x, y) => {
+    let sum = x + y;
+    return sum;
+}
+
+let res = SumOfTwoNumber(5,6);
+console.log(res);
